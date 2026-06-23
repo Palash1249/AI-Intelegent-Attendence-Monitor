395c4d29492c29730573e7855e5d2358dee5e8e8
Palash1249 <palashhalder10c18@gmail.com>
2026-06-23 07:30:07 +0530

    Improve dashboard UI and styling

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,17 +2,21 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
-    st.markdown (f"""
-        <div style='display:flex; flex-direction:column; align-items:center; justify-content:center;'>
-            <p style='font-weight:bold; color:white;'> Created with ❤️ by Palash Halder</p>
-    """, unsafe_allow_html=True)
-
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by Palash Halder</p>
+        </div>
+                
+                """, unsafe_allow_html=True)
 
 
 def footer_dashboard():
-    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
-    st.markdown (f"""
-        <div style='display:flex; flex-direction:column; align-items:center; justify-content:center;'>
-            <p style='font-weight:bold; color:black;'> Created with ❤️ by Palash Halder</p>
-    """, unsafe_allow_html=True)
+    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by Palash Halder</p>
+        </div>
+                
+                """, unsafe_allow_html=True)
--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -1,99 +1,145 @@
 import streamlit as st
 
-def style_background_layout():
+
+
+def style_background_home():
+
     st.markdown("""
         <style>
-            .stApp {
-                background-color: #5865F2 !important;
-            }
-            
-            .stApp div[data-testid="stColumn"]{
-                # color:#2f323f !important;
-                # background-color:#E0E3FF !important;
-                # padding:2.5rem !important;
-                # border-radius: 5rem !important;
 
-                background-color:#E0E3FF !important;
-                padding:2.5rem !important;
-                border-radius: 5rem !important;
-            }
-        </style>
-    """, unsafe_allow_html=True)
+                .stApp {
+                    background: #5865F2 !important;
+                }
+
+                .stApp div[data-testid="stColumn"]{
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius: 5rem !important;
+                    }
+                
+                .stApp div[data-testid="stColumn"] h2{
+                    color:#262730 !important;
+                }
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+    
 
 def style_background_dashboard():
-    st.markdown ('''
-        <style>
-            .stApp {
-                background: #E0E3FF !important;
-            }
-        </style>
-       ''', unsafe_allow_html=True)
 
-def style_base_layout():
     st.markdown("""
         <style>
-           @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
-            @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
-                    
-            /* Hide main NAV BAR OR HEAD BAR */
+                .stApp {
+                    background: #E0E3FF !important;
+                }
+
+                /* Login title */
+                div[data-testid="stHeading"] h1,
+                div[data-testid="stHeading"] h2 {
+                    color: #262730 !important;
+                }
+
+                /* Input labels */
+                label p {
+                    color: #262730 !important;
+                    font-weight: 600 !important;
+                }
+
+                /* Text input boxes */
+                .stTextInput input {
+                    background-color: #E0E3FF !important;
+                    color: #262730 !important;
+                    border: 1px solid #D5D9F5 !important;
+                    border-radius: 5px !important;
+                }
+
+                /* Placeholder text */
+                .stTextInput input::placeholder {
+                    color: #7A7D8C !important;
+                }
+
+                /* Sub Headder */
+                div[data-testid="stHeading"] h3 {
+                    color: #262730 !important;
+                }
+        </style>  
+
+                """
+            ,unsafe_allow_html=True)
+
+
+
+
+def style_base_layout():
+# asdasd
+    st.markdown("""
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+
                 
-                #MainMenu, footer, header {
-                    visibility: hidden;
+         /* Hide Top Bar of streamlit */
+                
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
+                
+            .block-container {
+                padding-top:1.5rem !important;    
+            }
+
+            h1 {
+                font-family: 'Climate Crisis', sans-serif !important;
+                font-size: 3.5rem !important;
+                line-height:1.1 1important;
+                margin-bottom:0rem !important;
+            }
+                
+
+            h2 {
+                font-family: 'Climate Crisis', sans-serif !important;
+                font-size: 2rem !important;
+                line-height:0.9 !important;
+                margin-bottom:0rem !important;
+            }
+                
+            h3, h4, p {
+                font-family: 'Outfit', sans-serif;    
+            }
+                
+
+            button{
+                border-radius: 1.5rem !important;
+                background-color: #5865F2 !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
                 }
 
-                .block-container{
-                    padding-top: .5rem !important;
+            button[kind="secondary"]{
+                border-radius: 1.5rem !important;
+                background-color: #EB459E !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
                 }
 
-                h1{
-                    font-family: 'Climate Crisis',sans-serif !important;
-                    font-size: 3.5rem !important;
-                    line-height: 1.1 !important;
-                    margin-bottom:0rem !important;
-                }
-                h2{
-                    font-family: 'Climate Crisis',sans-serif !important;
-                    font-size: 2rem !important;
-                    line-height: 0.9 !important;
-                    margin-bottom:0rem !important;
+            button[kind="tertiary"]{
+                border-radius: 1.5rem !important;
+                background-color: black !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
                 }
 
-                h3,h4,p{
-                    font-family: 'Outfit', sans-serif;
-                }
+            button:hover{
+                transform :scale(1.05)}
+        </style>  
 
-                button{
-                    border-radius: 1.5rem !important;
-                    background: #5865F2 !important;
-                    color: white !important;
-                    padding: 10px 20px !important;
-                    border: none !important;
-                    transition: transform 0.25s ease-in-out !important;
-                }
-                button[kind="secondary"]{
-                    border-radius: 1.5rem !important;
-                    background: #EB459E !important;
-                    color:white !important;
-                    padding: 10px 20px !important;
-                    border: none !important;
-                    transition: transform 0.25s ease-in-out !important;
-                }
-                button[kind="tertiary"]{
-                    border-radius: 1.5rem !important;
-                    background: black !important;
-                    color:white !important;
-                    padding: 10px 20px !important;
-                    border: none !important;
-                    transition: transform 0.25s ease-in-out !important;
-                }
-
-                button:hover{
-                    transform: scale(1.05);
-                }
-
-                .teacher-title{
-                    color:#2f323f !important;
-                }
-        </style>
-    """, unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
